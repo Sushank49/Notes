d5406520db71659cc7566d7de5fed78a86ec1298
HardikNeo <109664954+HardikNeo@users.noreply.github.com>
2023-06-01 15:03:43 +0545

    First commit.
    First back-end commit.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 8090;
+const port = 8000;
 
-app.set('view engine', 'pug');
-app.use(express.static('static'));
+app.set("view engine", "pug");
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    params = {
-      hi : "Hello topi"
-    }
-    res.status(200).render('index.pug', params);
-  });
-app.post('/', (req,res,next)=>{
-  console.log("Pakehhe")
-  console.log(req)
-})
+app.get("/", (req, res) => {
+  params = {
+    hi: "Hello topi",
+  };
+  res.status(200).render("index.pug", params);
+});
+app.post("/", (req, res, next) => {
+  console.log("Pakehhe");
+  console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log("Connected on http://127.0.0.1:8000");
