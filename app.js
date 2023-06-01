482652e69547d688146b7a9935676a8d62d4f7c0
HardikNeo <109664954+HardikNeo@users.noreply.github.com>
2023-06-01 17:37:43 +0545

    2nd commit: Few changes nothing important

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 8000;
 
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const MONGODB_URI =
+  "mongodb+srv://Notes:<notes.database>@cluster0.6dirlgp.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    // ... other options
+  })
+);
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+
 app.set("view engine", "pug");
 app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,9 +40,10 @@ app.get("/", (req, res) => {
   };
   res.status(200).render("index.pug", params);
 });
-app.post("/", (req, res, next) => {
+app.post("/login", (req, res, next) => {
   console.log("Pakehhe");
   console.log(req.body);
+  res.send(req.body);
 });
 
 app.listen(port, () => {
